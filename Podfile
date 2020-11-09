af465a318005f4bb0d785e17362ca4d5d4a83a43
byLXS <>
2020-11-10 00:51:29 +0300

    added kingfisher

--- Podfile
+++ Podfile
@@ -4,6 +4,16 @@ use_frameworks!
 
 workspace 'WalletChallenge-iOS'
 
+#core module
+def core_pods
+    pod 'Kingfisher'
+end
+ 
+target 'WalletCore' do
+   project 'WalletCore/WalletCore.project'
+  core_pods
+end
+
 #application module
 def application_pods
 end
@@ -11,5 +21,5 @@ end
 target 'WalletChallengeApp' do
    project 'WalletChallengeApp/WalletChallengeApp.project'
    application_pods
+   core_pods
 end
-
--- WalletCore/Source/Cache/ImageLoader.swift
+++ WalletCore/Source/Cache/ImageLoader.swift
@@ -2,15 +2,8 @@ import Foundation
 import UIKit.UIImage
 import Kingfisher
 
-
 public final class ImageLoader {
     public static let shared = ImageLoader()
-
-    private let cache: ImageCacheType
-
-    public init(cache: ImageCacheType = ImageCache()) {
-        self.cache = cache
-    }
 
     public func loadImage(from url: URL, completion: @escaping (UIImage?) -> ()) {
         KingfisherManager.shared.retrieveImage(with: url, options: nil, progressBlock: nil, completionHandler: { image, error, cacheType, imageURL in
